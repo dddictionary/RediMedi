4847255c6eec472daa82d02ea967487e54788be9
AliKhachab <a.khachab620@gmail.com>
2024-02-10 19:33:55 -0500

    login fixes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,106 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { FaUser, FaLock } from "react-icons/fa";
-window.fbAsyncInit = function () {
-  FB.init({
-    appId: "732440392382696",
-    cookie: true,
-    xfbml: true,
-    version: '19.0',
-  });
-
-  FB.AppEvents.logPageView();
-};
-
-(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
-  }
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
-
-const pingBackend = () => {
-    fetch('/auth/facebook');
-}
-
-// function checkLoginState() {
-//   FB.getLoginStatus(function (response) {
-//     statusChangeCallback(response);
-//   });
-// }
-
-// FB.getLoginStatus(function (response) {
-//   statusChangeCallback(response);
-// });
 
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="body">
-        <div className="login">
-      <form className="form" action="">
-        <h1 className="login__header">Login</h1>
-        <div className="login__username-wrapper">
-          <input
-            className="username-text"
-            type="text"
-            placeholder="Username"
-            required
-          />
-          <FaUser className="icon" />
-        </div>
-        <div className="login__password-wrapper">
-          <input
-            className="password-text"
-            type="text"
-            placeholder="Password"
-            required
-          />
-          <FaLock className="icon" />
-        </div>
-        <div className="login__rmm-forgot">
-          <label>
-            <input type="checkbox" /> Remember me
-          </label>
-          <a href="#" className="login__rmm-forgot__link">
-            Forgot password?
-          </a>
-        </div>
-        <button class="submit">Login</button>
-        <div className="login__register">
-          <p className="register-text">
-            Don't have an account?{" "}
-            <a href="#" className="register-link">
-              Register here
+      <div className="login">
+        <form className="form" action="">
+          <h1 className="login__header">Login</h1>
+          <div className="login__username-wrapper">
+            <input
+              className="username-text"
+              type="text"
+              placeholder="Username"
+              required
+            />
+            <FaUser className="icon" />
+          </div>
+          <div className="login__password-wrapper">
+            <input
+              className="password-text"
+              type={showPassword ? "text" : "password"} // Toggle password visibility
+              placeholder="Password"
+              required
+            />
+            <FaLock className="icon" />
+            <input
+              type="checkbox"
+              id="showPassword"
+              checked={showPassword}
+              onChange={handleTogglePassword}
+            />
+            <label class="showPassword" htmlFor="showPassword">{" "}Show Password</label>
+          </div>
+          <div className="login__forgot">
+            <a href="#" className="login__forgot__link">
+              Forgot password?
             </a>
-          </p>
-        </div>
-      </form>
-      <div id="fb-root"></div>
-      <script
-        async
-        defer
-        crossorigin="anonymous"
-        src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v19.0&appId=732440392382696"
-        nonce="oAyole8E"
-      ></script>
-      <div
-        class="fb-login-button"
-        data-width="200px"
-        data-size=""
-        data-button-type=""
-        data-layout=""
-        data-auto-logo
-        ut-link="true"
-        data-use-continue-as="false"
-        onClick={{pingBackend}}
-      ></div>
+          </div>
+          <label>
+            <input className="submit" type="submit" value="Submit" />
+          </label>
+          <div className="login__register">
+            <p className="register-text">
+              Don't have an account?{" "}
+              <a href="#" className="register-link">
+                Register here
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
-    </div>
-    
   );
 }
