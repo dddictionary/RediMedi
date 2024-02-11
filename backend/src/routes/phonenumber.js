807b7bc29c41c8064fd21bf374b22fac7b162a12
Imtiaz <imtiazc56@gmail.com>
2024-02-11 01:25:13 -0500

    Added Twilio to phonenumber.js to test sms functions

--- backend/src/routes/phonenumber.js
+++ backend/src/routes/phonenumber.js
@@ -1,10 +1,17 @@
 import express from "express";
 import PhoneNumber from "../models/phoneNumberSchema.js";
+import twilio from 'twilio';
 
 const router = express.Router();
 
 // Use express.json() middleware to parse JSON in the request body
 router.use(express.json());
+
+// Twilio credentials
+const accountSid = 'AC824f001a8cf88aae84aefdc509f52e37';
+const authToken = '21a4c68c910bf2ac1b3be3277e1ec00d';
+const twilioClient = twilio(accountSid, authToken);
+const twilioPhoneNumber = '7183109583';
 
 router.post('/phonenumber', async (req, res) => {
     try {
@@ -14,6 +21,16 @@ router.post('/phonenumber', async (req, res) => {
             phoneNumber,
         });
         await newPhoneNumber.save();
+
+        // Send SMS remainders
+        const message = await twilioClient.messages.create({
+            body: 'Your Refill is Due Soon. Please Schedule a Refill Appointment.',
+            from: twilioPhoneNumber,
+            to: phoneNumber,
+        });
+
+        console.log('SMS sent:', message.sid);
+        
         res.status(201).json({ message: 'Phone Number Saved Successfully' });
     } catch (err) {
         console.error(err);
